cbb291720e783f34ba1ddaa952ecdce41c1e0999
reinikeda <reinikeda@gmail.com>
2023-03-17 12:04:15 +0200

    Count in observation form restriction to 1 or more

--- faunaround/faunaweb/forms.py
+++ faunaround/faunaweb/forms.py
@@ -1,11 +1,13 @@
 from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django.utils import timezone
 from django_select2.forms import Select2Widget
+from django.core.validators import MinValueValidator
 from django import forms
 from .models import Observation
 
 
 class ObservationForm(forms.ModelForm):
+    count = forms.IntegerField(validators=[MinValueValidator(1)], min_value=1, initial=1)
     class Meta:
         model = Observation
         fields = ['date', 'species', 'count', 'place', 'photo']
